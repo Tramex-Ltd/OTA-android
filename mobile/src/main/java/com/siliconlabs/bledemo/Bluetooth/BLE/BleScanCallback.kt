37a8600ac987559eae8b35da5028897684cad63c
Lee <lee@tramexmeters.com>
2024-10-01 13:53:14 +0100

    Remove unnecessary ota ui elements

--- mobile/src/main/java/com/siliconlabs/bledemo/Bluetooth/BLE/BleScanCallback.kt
+++ mobile/src/main/java/com/siliconlabs/bledemo/Bluetooth/BLE/BleScanCallback.kt
@@ -17,48 +17,21 @@ import java.util.HexFormat
 class BleScanCallback(private val service: BluetoothService) : ScanCallback() {
     private val handler: Handler = Handler(Looper.getMainLooper())
 
-    // override fun onScanResult(callbackType: Int, result: ScanResult) {
-    //     Timber.d( "onScanResult: $result")
-    //     service.handleScanCallback(ScanResultCompat.from(result))
-    // }
-
-//    fun ByteArray.toHexString(): String = joinToString("") { "%02x".format(it) }
-
     override fun onScanResult(callbackType: Int, result: ScanResult?) {
         super.onScanResult(callbackType, result);
 
         result?.scanRecord?.manufacturerSpecificData?.let { manufacturerData ->
             val advertisingBytes = result?.scanRecord?.bytes
             if(manufacturerData.size() > 0 && manufacturerData.keyAt(0) == 1450){
-                Timber.d("YAY")
-
+                //Timber.d("YAY")
                 advertisingBytes?.let { bytes ->
-                    if (bytes.size >= 25 && bytes[25].toInt() == 82) { // Check if the array has at least 11 bytes
-                        service.handleScanCallback(ScanResultCompat.from(result))
-                        //                        val byte10 = bytes[25] // Access the 11th byte (index 10)
-//                        Timber.d(byte10.toString(16).padStart(2, '0'))
-//                        Timber.d("%02x".format(byte10.toInt() and 0xFF))
-                        Timber.d(bytes[25].toString())
-                        // ... use byte10 ...
+                    if (bytes.size >= 25 && bytes[25].toInt() == 82) { // Check if the array has at least 25 bytes
+                        service.handleScanCallback(ScanResultCompat.from(result))// Only handle callback on the device we want to update
                     }
                 }
-
-//                advertisingBytes?.let {  // Use let block to handle null case
-//                    var counter = 0
-//                    for (b in it) {
-//                        counter ++
-//                        Timber.d(counter.toString())
-////                        Timber.d(b.toString(16).padStart(2, '0'))
-//                        Timber.d("%02x".format(b.toInt() and 0xFF))
-//                    }
-//                }
-//                service.handleScanCallback(ScanResultCompat.from(result))
             }
         }
     }
-
-
-
 
     override fun onBatchScanResults(results: List<ScanResult>) {
         results.forEach {
